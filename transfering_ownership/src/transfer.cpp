c77ef55b8b6aa467cf658d805ad5a80c438999be
Boris Egorov <jightuse@gmail.com>
2013-11-19 16:19:32 +0700

    Add transfering ownership of a thread example
    Initial code: shows transfering of ownership of threads among three
    std::thread instances.

---
+++ transfering_ownership/src/transfer.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <thread>
+#include <unistd.h>
+
+void some_function(int i)
+{
+    const int sleep_time = 6;
+    std::cout << "thread " << i << " started." << std::endl;
+    sleep(sleep_time);
+    std::cout << "thread " << i << " ended." << std::endl;
+}
+
+void some_other_function(int i)
+{
+    const int sleep_time = 3;
+    std::cout << "thread " << i << " started." << std::endl;
+    sleep(sleep_time);
+    std::cout << "thread " << i << " ended." << std::endl;
+}
+
+
+int main()
+{
+    std::thread t1(some_function, 1);
+    std::thread t2=std::move(t1);
+    t1 = std::thread(some_other_function, 2);
+    std::thread t3;
+    t3 = std::move(t2);
+    // next assignment terminate program because t1 already had an
+    // associated thread
+    //t1 = std::move(t3);
+    t1.join();
+    t3.join();
+}
